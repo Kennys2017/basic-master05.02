3d3ac7628677b70eb31813287f80776f02cb1da3
Kennys2017 <gameblogchanell@mail.ru>
2023-04-09 19:49:38 +0300

    New one

--- views/site/index.php
+++ views/site/index.php
@@ -4,12 +4,16 @@
 
 $this->title = 'No Ozone';
 ?>
+    <div class="colummn">
+        <h2 style="margin-top:30px; text-align:center;">Страйкбольный магазин AirWars- всё для страйкбола </h2>
+        <img src="../sales.jpg" alt="Баннер акции" style="height:300px; margin-left:300px; margin-top:30px">
+    </div>
 
 <div class="row">
     <?php
         foreach($post as $posts){
             echo '
-            <div  style="margin-top:100px;border:solid; border-color:grey;" class="col-lg-3">
+            <div  style="margin-top:50px;border:solid; border-color:grey;" class="col-lg-3">
             <h2 style="text-align:center">'.$posts->name.'</h2>
 
             <img class="img-responsive" style="height:200px; margin-left:70px; " alt="Фото товара"  src="uploads/'.$posts->image.'">
